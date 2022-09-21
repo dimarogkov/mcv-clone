8c4f905714fcbe28146035e6986c600c446da3ef
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2022-09-22 02:21:15 +0300

    add yourFedd and tagFeed

---
+++ src/views/TagFeed.vue
@@ -0,0 +1,37 @@
+<template>
+    <mcv-banner />
+
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-9">
+                    <mcv-feed :api-url="apiUrl" />
+                </div>
+                <div class="col-3">
+                    <mcv-tags />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import McvBanner from '@/components/Banner';
+import McvFeed from '@/components/Feed';
+import McvTags from '@/components/Tags';
+
+export default {
+    name: 'mcv-tag-feed',
+    components: {
+        McvBanner,
+        McvFeed,
+        McvTags,
+    },
+    computed: {
+        apiUrl() {
+            const tagName = this.$route.params.slug;
+            return `/articles?tag=${tagName}`;
+        },
+    },
+};
+</script>
---
+++ src/views/YourFeed.vue
@@ -0,0 +1,36 @@
+<template>
+    <mcv-banner />
+
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-9">
+                    <mcv-feed :api-url="apiUrl" />
+                </div>
+                <div class="col-3">
+                    <mcv-tags />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import McvBanner from '@/components/Banner';
+import McvFeed from '@/components/Feed';
+import McvTags from '@/components/Tags';
+
+export default {
+    name: 'mcv-your-feed',
+    components: {
+        McvBanner,
+        McvFeed,
+        McvTags,
+    },
+    data() {
+        return {
+            apiUrl: '/articles/feed',
+        };
+    },
+};
+</script>
